ecb1d79acce51949413d23b7a2a2ec5f0c99ed1e
dipak <sarkar3115@gmail.com>
2023-04-10 15:22:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import './App.scss'
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(
+  BarElement,
+  CategoryScale, 
+  LinearScale, 
+  Tooltip, 
+  Legend
+)
 function App() {
+  const data = {
+    labels: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+    datasets: [
+      {
+        label:'',
+        data:[17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+        backgroundColor: ["#ec775f","#ec775f","#76b5bc","#ec775f","#ec775f","#ec775f","#ec775f"],
+        borderRadius: 4
+      }
+    ]
+  }
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+          display: false,
+      }
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        display: false,
+        grid: {
+          display: false
+        }
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='expense-card'>
+      <section className='ec-header'>
+        <div className='ec-balance'>
+          <p>My balance</p>
+          <h1>$921.48</h1>
+        </div>
+        <img src={logo} alt="logo" />
+      </section>
+      <section className='ec-main'>
+        <section className='ec-chart'>
+          <strong>Spending - Last 7 days</strong>
+          <Bar 
+            data={data} options={options}>
+          </Bar>
+        </section>
+        <hr></hr>
+        <section className='ec-total'>
+          <div className='ec-total-month'>
+            <p>Total this month</p>
+            <strong>$478.33</strong>
+          </div>
+          <div className='ec-total-diff'>
+            <strong>+2.4%</strong>
+            <p>from last month</p>
+          </div>
+        </section>
+      </section>
+    </article>
   );
 }
 
